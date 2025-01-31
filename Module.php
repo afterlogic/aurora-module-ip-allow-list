80e2a9dc186d50b547b24ffa1ca097995252397a
sash04ek <a.ovcharov@gmail.com>
2025-01-31 16:50:09 +0300

    Remove GetTenantWithoutRoleCheck and GetUserWithoutRoleCheck

--- Module.php
+++ Module.php
@@ -185,7 +185,7 @@ class Module extends \Aurora\System\Module\AbstractModule
         if (isset($aArgs['Login']) && isset($aArgs['Password'])) {
             $aAuthData = \Aurora\Modules\Core\Module::Decorator()->Authenticate($aArgs['Login'], $aArgs['Password']);
             if (is_array($aAuthData) && isset($aAuthData['id'])) {
-                $oUser = \Aurora\Modules\Core\Module::Decorator()->GetUserWithoutRoleCheck($aAuthData['id']);
+                $oUser = \Aurora\Api::getUserById($aAuthData['id']);
                 if ($oUser) {
                     \Aurora\Api::skipCheckUserRole(true);
                     $this->checkIpAddress($oUser);
